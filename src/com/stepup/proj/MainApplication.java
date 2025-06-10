766d8c57cf687fce91af19ca1417670b0143c27d
yaregtlt <redin-yaroslav-v@yandex.ru>
2025-06-10 22:02:18 +0400

    Some changes

--- src/com/stepup/proj/MainApplication.java
+++ src/com/stepup/proj/MainApplication.java
@@ -2,9 +2,8 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world");
         System.out.println("Hello world!");
         System.out.println("Hello world!!");
-        System.out.println("Hello world!!!");
+        System.out.println("Hello dummy!!!");
     }
 }
