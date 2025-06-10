39081d555b4d9b7ac2ad57712542b537a48c1d49
yaregtlt <redin-yaroslav-v@yandex.ru>
2025-06-10 21:32:11 +0400

    Main modification

--- src/com/stepup/proj/MainApplication.java
+++ src/com/stepup/proj/MainApplication.java
@@ -3,5 +3,8 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
+        System.out.println("Hello world!!!");
     }
 }
